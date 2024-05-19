c52f6eba57567734b88fbeb409c4b59a40c69e7e
Bilal Mumtaz <bilalmumtaz868@gmail.com>
2024-05-19 21:09:06 +0500

    forgot password student, verify page protected, admin dashboard bug fixed

--- src/pages/EmailVerificationStudent/VerificationPage.jsx
+++ src/pages/EmailVerificationStudent/VerificationPage.jsx
@@ -1,7 +1,18 @@
+import  { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import ValidationBox from "./ValidationBox";
 import VerificationPageNavbar from "./VerificationPageNavbar";
 
 function VerificationPage() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const email = localStorage.getItem("email");
+    if (!email) {
+      navigate("/login");
+    }
+  }, [navigate]);
+
   return (
     <>
       <VerificationPageNavbar currentImageIndex={0} />
--- src/pages/mainPage/Tutor-Search/TutorsSearch.jsx
+++ src/pages/mainPage/Tutor-Search/TutorsSearch.jsx
@@ -2,7 +2,7 @@
 import { useEffect, useRef } from "react";
 import { useState } from "react";
 import Select from "react-select";
-import { useSearchParams } from 'react-router-dom';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 import NavBar from "../../../ui/NavBar";
 import AlternativeNavbar from "../../../ui/AlternativeNavbar";
 import './TutorSearch.css';
@@ -556,37 +556,37 @@ function TutorsSearch() {
                 console.error("Mutation failed:", error);
             });
     };
-    // const navigate = useNavigate();
-    // const handleOpenChat = async (teacherId)=>{
-    //     const userDataString = localStorage.getItem('user');
-    //     const userData = JSON.parse(userDataString);
-    //     const studentId = userData._id;
-    //     try {
-    //         setSkeltonLoading(true);
-    //         const response = await fetch('http://localhost:8080/createConversation', {
-    //           method: 'POST',
-    //           headers: {
-    //             'Content-Type': 'application/json',
-    //           },
-    //           body: JSON.stringify({ teacherId, studentId }),
-    //         });
+    const navigate = useNavigate();
+    const handleOpenChat = async (teacherId)=>{
+        const userDataString = localStorage.getItem('user');
+        const userData = JSON.parse(userDataString);
+        const studentId = userData._id;
+        try {
+            setSkeltonLoading(true);
+            const response = await fetch('http://localhost:8080/createConversation', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({ teacherId, studentId }),
+            });
       
-    //         if (response.ok) {
-    //           const data = await response.json();
-    //           // Assuming the conversation ID is returned in the response
-    //           const conversationId = data._id;
-    //           // Navigate to the chat page with the conversation ID
-    //             navigate(`/studentdashboard/chat/${conversationId}`);
+            if (response.ok) {
+              const data = await response.json();
+              // Assuming the conversation ID is returned in the response
+              const conversationId = data._id;
+              // Navigate to the chat page with the conversation ID
+                navigate(`/studentdashboard/chat/${conversationId}`);
 
-    //         } 
-    //       } catch (error) {
-    //         toast.error('Error creating conversation:',error);
+            } 
+          } catch (error) {
+            toast.error('Error creating conversation:',error);
 
-    //         console.error('Error creating conversation:', error);
-    //       } finally {
-    //         setSkeltonLoading(false);
-    //       }
-    // }
+            console.error('Error creating conversation:', error);
+          } finally {
+            setSkeltonLoading(false);
+          }
+    }
 
     return (
         <>
